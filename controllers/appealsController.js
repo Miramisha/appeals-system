d198d5797e514fe80cdc61a34b0daa84905aff55
Fokasi_Twix <karina.kuslina.04@bk.ru>
2025-05-29 21:49:47 +0300

    Изменение метода getAppeals в файле appealsController.js

--- controllers/appealsController.js
+++ controllers/appealsController.js
@@ -45,10 +45,10 @@ exports.cancelAppeal = async (req, res) => {
 
 exports.getAppeals = async (req, res) => {
   const { date, start, end, page = 1, limit = 50 } = req.query;
-  const safeLimit = Math.min(parseInt(limit), 100); // максимум 100 записей за раз
-  const offset = (parseInt(page) - 1) * safeLimit;
+  const safeLimit = Math.min(parseInt(limit), 100); // максимум 100 записей
 
   try {
+    const offset = (page - 1) * safeLimit;
     const [rows] = await model.getAppeals(date || start, date ? date : end, safeLimit, offset);
     res.json(rows);
   } catch (err) {
